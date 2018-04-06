e40e33d3c58da97ad25db61fd9421a9fc7231206
3mey <3mey@ukr.net>
2018-04-05 22:22:53 -0700

    Integrated swagger UI for documenting and and executing our API.

--- src/main/java/com/qodemy/classroom/controller/UserController.java
+++ src/main/java/com/qodemy/classroom/controller/UserController.java
@@ -3,14 +3,15 @@ package com.qodemy.classroom.controller;
 
 import com.qodemy.classroom.model.User;
 import com.qodemy.classroom.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 /**
@@ -19,13 +20,20 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/users")
+@Api(tags = {"User"}, value = "user", description = "User API")
 public class UserController {
-
 
     @Autowired
     private UserService userService;
 
     // =========================================== Create New User ========================================
+
+    @ApiOperation(value = "This Rest API is used to create a user.",
+            notes = "This API will create a new user.",
+            response = User.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Successfully created a new user", response = User.class)
+    })
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<Void> create(@RequestBody User user, UriComponentsBuilder ucBuilder) {
 
